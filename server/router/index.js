a2a0e181fa5ebc02529c3a313dbcef4d577b4f50
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-26 23:47:40 -0300

    add report path

--- server/router/index.js
+++ server/router/index.js
@@ -5,6 +5,7 @@ router.get('/api/people.json', PeopleController.get)
 router.get('/api/people/:id.json', PeopleController.getById)
 
 router.post('/api/people.json', PeopleController.create)
+router.post('/api/people/:id/report_infection.json', PeopleController.report)
 
 router.patch('/api/people/:id.json', PeopleController.update)
 
