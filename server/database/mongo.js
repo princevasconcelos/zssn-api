4a706e2f893d5ed502a0f913fee8ea84139a9614
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-26 23:47:02 -0300

    add connection feedback

--- server/database/mongo.js
+++ server/database/mongo.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/zssn', { useNewUrlParser: true })
-
 mongoose.Promise = global.Promise
+
+const connection = mongoose.connection
+connection.on('error', console.error.bind(console, 'connection error:'))
+connection.once('open', function () {
+  console.log('Mongoose connection successful')
+})
 
 module.exports = mongoose
