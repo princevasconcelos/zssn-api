5dec8739fee54f997240f2004bb2017a913bb1db
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-28 04:16:40 -0300

    fix useCreateIndex

--- server/database/mongo.js
+++ server/database/mongo.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/zssn', { useNewUrlParser: true })
 mongoose.Promise = global.Promise
+mongoose.set('useCreateIndex', true)
 
 const connection = mongoose.connection
 connection.on('error', console.error.bind(console, 'connection error:'))
