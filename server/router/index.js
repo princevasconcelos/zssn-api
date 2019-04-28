57fb1775f0b84fb20cfeb0fc4590a21358e7bd04
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-28 04:18:06 -0300

    add trade and getItem routes

--- server/router/index.js
+++ server/router/index.js
@@ -3,9 +3,14 @@ const PeopleController = require('../controllers/PeopleController')
 
 router.get('/api/people.json', PeopleController.get)
 router.get('/api/people/:id.json', PeopleController.getById)
+router.get('/api/people/:id/properties.json', PeopleController.getItems)
 
 router.post('/api/people.json', PeopleController.create)
 router.post('/api/people/:id/report_infection.json', PeopleController.report)
+router.post(
+  '/api/people/:id/properties/trade_item.json',
+  PeopleController.trade
+)
 
 router.patch('/api/people/:id.json', PeopleController.update)
 
