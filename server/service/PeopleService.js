52107f33457c5d8211205e60740aa31ae41bc951
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-29 04:54:03 -0300

    add removeUndefineds

--- server/service/PeopleService.js
+++ server/service/PeopleService.js
@@ -1,11 +1,13 @@
 const People = require('../model/people')
 
+const removeUndefineds = myObj => JSON.parse(JSON.stringify(myObj))
+
 const PeopleService = {
   create: data => People.create(data),
   getAll: () => People.find(),
   getById: id => People.findById(id),
   update: (id, data) =>
-    People.findByIdAndUpdate(id, data, {
+    People.findByIdAndUpdate(id, removeUndefineds(data), {
       new: true
     })
 }
